e7cafbdcfb39d5a82494a81ec4847153629211ae
dedbot365 <poudelsubigyan73@gmail.com>
2025-01-04 15:01:02 +0545

    Popup Windows Added

--- expenzo/Models/Debt.cs
+++ expenzo/Models/Debt.cs
@@ -4,6 +4,7 @@ namespace expenzo.Models
 {
     public class Debt
     {
+        public int DebtId { get; set; }
         public decimal DebtAmount { get; set; }
         public DateTime DebtTakenDate { get; set; } = DateTime.Now;
         public DateTime DebtDueDate { get; set; }
