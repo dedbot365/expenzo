2efe7376e4d92848d8caf31629c262e6a85ee9b1
dedbot365 <poudelsubigyan73@gmail.com>
2025-01-14 17:43:01 +0545

    Minor updates

--- expenzo/Services/EncryptionService.cs
+++ expenzo/Services/EncryptionService.cs
@@ -2,61 +2,75 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace expenzo.Services
+namespace expenzo.Services;
+public class EncryptionService
 {
-    public class EncryptionService
-    {
-        private readonly byte[] key;
-        private readonly byte[] iv;
+    private static byte[] key = Array.Empty<byte>();
+    private static byte[] iv = Array.Empty<byte>();
 
-        public EncryptionService()
+    static EncryptionService()
+    {
+        // Load key and IV from storage
+        LoadKeyAndIV();
+    }
+
+    private static void LoadKeyAndIV()
+    {
+        
+        if (File.Exists("key.bin") && File.Exists("iv.bin"))
         {
-            // Generate a key and IV for AES encryption
+            key = File.ReadAllBytes("key.bin");
+            iv = File.ReadAllBytes("iv.bin");
+        }
+        else
+        {
             using (var aes = Aes.Create())
             {
                 key = aes.Key;
                 iv = aes.IV;
+                File.WriteAllBytes("key.bin", key);
+                File.WriteAllBytes("iv.bin", iv);
             }
         }
+    }
 
-        public string Encrypt(string plainText)
+    public string Encrypt(string plainText)
+    {
+        using (var aes = Aes.Create())
         {
-            using (var aes = Aes.Create())
-            {
-                aes.Key = key;
-                aes.IV = iv;
+            aes.Key = key;
+            aes.IV = iv;
 
-                var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-                using (var ms = new System.IO.MemoryStream())
+            var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
                 {
-                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    using (var sw = new StreamWriter(cs))
                     {
-                        using (var sw = new System.IO.StreamWriter(cs))
-                        {
-                            sw.Write(plainText);
-                        }
-                        return Convert.ToBase64String(ms.ToArray());
+                        sw.Write(plainText);
                     }
+                    return Convert.ToBase64String(ms.ToArray());
                 }
             }
         }
+    }
 
-        public string Decrypt(string cipherText)
+    public string Decrypt(string cipherText)
+    {
+        using (var aes = Aes.Create())
         {
-            using (var aes = Aes.Create())
-            {
-                aes.Key = key;
-                aes.IV = iv;
+            aes.Key = key;
+            aes.IV = iv;
 
-                var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-                using (var ms = new System.IO.MemoryStream(Convert.FromBase64String(cipherText)))
+            var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            using (var ms = new MemoryStream(Convert.FromBase64String(cipherText)))
+            {
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
                 {
-                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    using (var sr = new StreamReader(cs))
                     {
-                        using (var sr = new System.IO.StreamReader(cs))
-                        {
-                            return sr.ReadToEnd();
-                        }
+                        return sr.ReadToEnd();
                     }
                 }
             }
