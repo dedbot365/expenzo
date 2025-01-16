e67f96bee70af8b44698934fb31cae63614403b3
dedbot365 <poudelsubigyan73@gmail.com>
2025-01-16 23:23:47 +0545

    database context edited

--- expenzo/Database/DatabaseContext.cs
+++ expenzo/Database/DatabaseContext.cs
@@ -8,20 +8,20 @@ public class DatabaseContext
 
     public DatabaseContext()
 {
-    try
-    {
+    
+    
         // Define the path for the database in the local application data folder
         var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         var folderPath = Path.Combine(appDataPath, "expenzo");
         Directory.CreateDirectory(folderPath); // Create directory if it doesn't exist
         _databasePath = Path.Combine(folderPath, "expenzo.db");
-    }
-    catch (Exception ex)
-    {
-        // Log the exception (you can use any logging framework or simply write to the console)
-        Console.WriteLine($"An error occurred while creating the database path: {ex.Message}");
-        throw; 
-    }
+    
+    // catch (Exception ex)
+    // {
+    //     // Log the exception (you can use any logging framework or simply write to the console)
+    //     Console.WriteLine($"An error occurred while creating the database path: {ex.Message}");
+    //     throw; 
+    // }
 }
 
     public SqliteConnection GetConnection()
